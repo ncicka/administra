3219802db7a98a8e02bee8b3d988a2d22335e76e
ncicka <77896911+ncicka@users.noreply.github.com>
2024-04-17 17:37:58 -0300

    Update b.bat

--- Datos/b.bat
+++ Datos/b.bat
@@ -3,4 +3,4 @@ FOR /F "tokens=1,2,3 delims=/ " %%i IN ('date /T') do (set DIA=%%k%%j%%i)
 FOR /F "tokens=1,2 delims=: " %%n IN ('time /T') do (set HORA=%%n%%o)
 
 
-C:\"Program Files (x86)"\MySQL\"MySQL Server 5.5"\bin\mysqldump -u mimi -pmimi administra > "c:\administra_%DIA%_%HORA%.sql"
+C:\"Program Files (x86)"\MySQL\"MySQL Server 5.5"\bin\mysqldump -u .imi -p.imi administra > "c:\administra_%DIA%_%HORA%.sql"
